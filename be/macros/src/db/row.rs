d226699cf0a1fdebb459f8c0737a4690d83c6de9
ianlau <ianlauyin@gmail.com>
2025-06-06 11:55:26 +0800

    be: merge derive Row with partial

--- be/macros/src/db/row.rs
+++ be/macros/src/db/row.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{Data, DataStruct, DeriveInput, Field, Fields, Ident, parse2};
+use syn::{Data, DataStruct, DeriveInput, Field, Fields, Ident, Type, parse2};
 
 pub fn derive_row(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = parse2(input).unwrap();
@@ -11,23 +11,29 @@ pub fn derive_row(input: TokenStream) -> TokenStream {
     let RowAttr {
         primary_key,
         field_idents,
+        field_pairs,
     } = derive_fields(fields);
     let from_row_tokens = from_row_tokens(row, &field_idents);
     let row_tokens = row_tokens(row, primary_key, &field_idents);
+    let partial_tokens = partial_tokens(row, primary_key, field_pairs);
+
     quote! {
         #from_row_tokens
         #row_tokens
+        #partial_tokens
     }
 }
 
 struct RowAttr<'a> {
     primary_key: &'a Field,
     field_idents: Vec<&'a Ident>,
+    field_pairs: Vec<(&'a Ident, &'a Type)>,
 }
 
 fn derive_fields(fields: &Fields) -> RowAttr {
     let mut primary_key_op = None;
     let mut field_idents = Vec::new();
+    let mut field_pairs = Vec::new();
     for field in fields {
         if let Some(attr) = field.attrs.get(0) {
             if attr.path().is_ident("primary_key") {
@@ -35,11 +41,13 @@ fn derive_fields(fields: &Fields) -> RowAttr {
             }
         }
         field_idents.push(field.ident.as_ref().unwrap());
+        field_pairs.push((field.ident.as_ref().unwrap(), &field.ty));
     }
     let primary_key = primary_key_op.expect("primary key is missing");
     RowAttr {
         primary_key,
         field_idents,
+        field_pairs,
     }
 }
 
@@ -68,62 +76,48 @@ fn row_tokens(row: &Ident, primary_key: &Field, field_idents: &Vec<&Ident>) -> T
             fn fields() -> Vec<String> {
                 vec![#(#field_names.to_string()),*]
             }
-            fn to_params(self) -> impl Into<lan_be_frame::mysql::Params> {
-                (#(self.#field_idents),*)
-            }
         }
     }
 }
 
-// use proc_macro2::TokenStream;
-// use quote::quote;
-// use syn::{Data, DataStruct, DeriveInput, Field, Ident, parse2};
+fn partial_tokens(
+    row_ident: &Ident,
+    primary_key: &Field,
+    field_pairs: Vec<(&Ident, &Type)>,
+) -> TokenStream {
+    let partial_row_ident = Ident::new(&format!("Partial{}", row_ident), row_ident.span());
+    let filter_field_pairs: Vec<(&Ident, &Type)> = field_pairs
+        .into_iter()
+        .filter(|(ident, _)| ident.to_string() != primary_key.ident.as_ref().unwrap().to_string())
+        .collect();
+    let (partial_field_tokens, into_partial_field_tokens) = field_tokens(&filter_field_pairs);
+    quote! {
+        pub struct #partial_row_ident {
+            #(#partial_field_tokens),*
+        }
 
-// pub fn derive_partial(input: TokenStream) -> TokenStream {
-//     let ast: DeriveInput = parse2(input).unwrap();
-//     let row_ident = &ast.ident;
-//     let partial_row_ident = Ident::new(&format!("Partial{}", row_ident), row_ident.span());
-//     let Data::Struct(DataStruct { fields, .. }) = &ast.data else {
-//         panic!("Row must be a struct");
-//     };
+        impl Into<#partial_row_ident> for #row_ident {
+            fn into(self) -> #partial_row_ident {
+                #partial_row_ident {
+                    #(#into_partial_field_tokens),*
+                }
+            }
+        }
 
-//     let ((partial_field_tokens, into_partial_field_tokens), to_data_fields_tokens): (
-//         (Vec<TokenStream>, Vec<TokenStream>),
-//         Vec<TokenStream>,
-//     ) = fields.iter().map(field_tokens).unzip();
+        impl lan_be_frame::db::Partial for #partial_row_ident {
+            type Row = #row_ident;
+        }
+    }
+}
 
-//     quote! {
-//         pub struct #partial_row_ident {
-//             #(#partial_field_tokens),*
-//         }
-
-//         impl Into<#partial_row_ident> for #row_ident {
-//             fn into(self) -> #partial_row_ident {
-//                 #partial_row_ident {
-//                     #(#into_partial_field_tokens),*
-//                 }
-//             }
-//         }
-
-//         impl lan_be_frame::db::partial for #partial_row_ident {
-//             type Row = #row_ident;
-//             fn into(self) -> Result<Self::Row, std::io::Error> {
-//                 Ok(#row_ident {
-//                     #(#to_data_fields_tokens),*
-//                 })
-//             }
-//         }
-//     }
-// }
-
-// fn field_tokens(field: &Field) -> ((TokenStream, TokenStream), TokenStream) {
-//     let field_ident = field.ident.as_ref().unwrap();
-//     let field_type = &field.ty;
-//     let partial_field_token = quote! (pub #field_ident: Option<#field_type>);
-//     let into_partial_field_token = quote! (#field_ident: Some(self.#field_ident));
-//     let to_data_fields_token = quote! (#field_ident: Self::unwrap_data(self.#field_ident)?);
-//     (
-//         (partial_field_token, into_partial_field_token),
-//         to_data_fields_token,
-//     )
-// }
+fn field_tokens(field_pairs: &Vec<(&Ident, &Type)>) -> (Vec<TokenStream>, Vec<TokenStream>) {
+    let mut partial_field_tokens = Vec::new();
+    let mut into_partial_field_tokens = Vec::new();
+    for (ident, ty) in field_pairs {
+        let partial_field_token = quote! (pub #ident: Option<#ty>);
+        let into_partial_field_token = quote! (#ident: Some(self.#ident));
+        partial_field_tokens.push(partial_field_token);
+        into_partial_field_tokens.push(into_partial_field_token);
+    }
+    (partial_field_tokens, into_partial_field_tokens)
+}
--- be/src/db/table/row.rs
+++ be/src/db/table/row.rs
@@ -1,19 +1,14 @@
-use mysql::{Params, Value, prelude::FromRow};
-use std::io::{Error, ErrorKind};
+use mysql::{Value, prelude::FromRow};
 
+/// Derive Row will also give you Partial<Row = Row>
 pub use lan_be_frame_macros::Row;
 
 pub trait Row: FromRow {
     type PKType: Into<Value>;
     fn pk() -> &'static str;
     fn fields() -> Vec<String>;
-    fn to_params(self) -> impl Into<Params>;
 }
 
 pub trait Partial {
     type Row: Row;
-    fn into(self) -> Result<Self::Row, Error>;
-    fn unwrap_data<T>(data: Option<T>) -> Result<T, Error> {
-        data.ok_or_else(|| Error::new(ErrorKind::InvalidData, "Missing data"))
-    }
 }
