e699414a0bce9bc84cf6ad3b8f7f28ee1b740d48
ianlau <ianlauyin@gmail.com>
2025-06-26 10:49:09 +0800

    update attributes for entity

--- examples/basic/src/db/user.rs
+++ examples/basic/src/db/user.rs
@@ -8,19 +8,3 @@ pub struct User {
     pub name: String,
     pub email: String,
 }
-
-// use lan_be_frame::sea_orm::entity::prelude::*;
-
-// #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-// #[sea_orm(table_name = "user")]
-// pub struct Model {
-//     #[sea_orm(primary_key)]
-//     id: u16,
-//     name: String,
-//     email: String,
-// }
-
-// impl ActiveModelBehavior for ActiveModel {}
-
-// #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-// pub enum Relation {}
--- macros/src/entity.rs
+++ macros/src/entity.rs
@@ -69,22 +69,35 @@ fn get_table_name(meta: &Meta) -> String {
 #[derive(Default)]
 struct ModelFieldAttr {
     primary_key: bool,
-    // nullable,
-    // auto_increment,
-    // unique,
-    // indexed,
-    // default_value,
+    nullable: bool,
+    unique: bool,
+    indexed: bool,
+    default_value: Option<String>,
+    auto_increment: Option<bool>,
 }
 
 impl Into<TokenStream> for ModelFieldAttr {
     fn into(self) -> TokenStream {
-        let mut tokens = quote!();
-        if self.primary_key {
-            tokens.extend(quote! {
-                #[sea_orm(primary_key)]
-            });
+        let mut tokens: Vec<TokenStream> = vec![];
+        self.primary_key.then(|| tokens.push(quote!(primary_key)));
+        self.nullable.then(|| tokens.push(quote!(nullable)));
+        self.unique.then(|| tokens.push(quote!(unique)));
+        self.indexed.then(|| tokens.push(quote!(indexed)));
+
+        if let Some(default_value) = self.default_value {
+            tokens.push(quote!(default_value = #default_value));
         }
-        tokens
+        if let Some(auto_increment) = self.auto_increment {
+            tokens.push(quote!(auto_increment = #auto_increment));
+        }
+        if tokens.is_empty() {
+            return quote!();
+        }
+        quote! {
+            #[sea_orm(
+                #(#tokens,)*
+            )]
+        }
     }
 }
 
@@ -122,10 +135,53 @@ fn get_model_field_attr(attrs: &[Attribute]) -> ModelFieldAttr {
     attrs.iter().for_each(
         |attr| match attr.path().get_ident().unwrap().to_string().as_str() {
             "primary_key" => {
+                check_meta_path("primary_key", &attr.meta);
                 model_field_attr.primary_key = true;
+            }
+            "nullable" => {
+                check_meta_path("nullable", &attr.meta);
+                model_field_attr.nullable = true;
+            }
+            "unique" => {
+                check_meta_path("unique", &attr.meta);
+                model_field_attr.unique = true;
+            }
+            "indexed" => {
+                check_meta_path("indexed", &attr.meta);
+                model_field_attr.indexed = true;
+            }
+            "default_value" => {
+                let lit = get_lit_from_meta_name_value("default_value", &attr.meta);
+                let Lit::Str(lit_str) = lit else {
+                    panic!("default_value must be a string");
+                };
+                model_field_attr.default_value = Some(lit_str.value());
+            }
+            "auto_increment" => {
+                let lit = get_lit_from_meta_name_value("auto_increment", &attr.meta);
+                let Lit::Bool(lit_bool) = lit else {
+                    panic!("auto_increment must be a boolean");
+                };
+                model_field_attr.auto_increment = Some(lit_bool.value());
             }
             other => panic!("Unsupported field attribute: {}", other),
         },
     );
     model_field_attr
+}
+
+fn check_meta_path(attr_name: &str, meta: &Meta) {
+    if !matches!(meta, Meta::Path(_)) {
+        panic!("{} must be not be list or name-value pair", attr_name);
+    }
+}
+
+fn get_lit_from_meta_name_value<'a>(attr_name: &str, meta: &'a Meta) -> &'a Lit {
+    let Meta::NameValue(MetaNameValue { value: expr, .. }) = &meta else {
+        panic!("{} must be a name-value pair", attr_name)
+    };
+    let Expr::Lit(ExprLit { lit, .. }) = &expr else {
+        panic!("value expr of {} must be a literal", attr_name);
+    };
+    lit
 }
